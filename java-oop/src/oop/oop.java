a39404f9b410c1fb51cb30dbfabee943197e5378
NA <nss7667@gmail.com>
2016-07-17 11:45:18 +0900

    2016.07.17 	Say Hello2! 추가.

--- java-oop/src/oop/oop.java
+++ java-oop/src/oop/oop.java
@@ -2,6 +2,7 @@ package oop;
 
 public class oop {
 	public static void main(String[] args) {
-		System.out.println("Say Hello!");
+		System.out.println("Say Hello1!");
+		System.out.println("Say Hello2!");
 	}
 }
